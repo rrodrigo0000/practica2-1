e8f4b67f87169f168ad4b30112555fd1adf811e7
Dankitan <maximo768@gmail.com>
2017-04-28 19:06:56 +0200

    Prototipos funcion y menu

---
+++ calculadora.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+
+float suma(float a, float b);
+float resta(float a, float b);
+double multiplicacion(float a, float b);
+float division(float a, float b);
+double potencia(float a, float b);
+double iva(float a);
+
+int main ( int argc, char *argv[]){
+     printf("----------------------------MENU--------------------------------"\n);
+     printf("1. Suma"\n);       
+     printf("2. Resta"\n);  
+     printf("3. Multiplicacion"\n);  
+     printf("4. Division"\n);  
+     printf("5. Potencia\n");
+     printf("6. Porcentaje\n");
+}
