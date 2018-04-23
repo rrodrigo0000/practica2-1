4a5ca7b44a5d8e423dc1fbf33b8c0d4006d067f3
MCarmenR <mcarmen.ruiz@uclm.es>
2018-04-23 16:28:41 +0000

    Revert "arreglado esqueleto"

--- calculadora.c
+++ calculadora.c
@@ -1,64 +1,188 @@
-#include <stdio.h>
+/* Calculadora */
 
-int main(int argc, char **argv)
+#include<stdio.h>
+
+#include<conio.h>
+
+#include<math.h>
+
+ 
+
+main()
+
 {
-	int a, b, result, option = -1;
-	// Mientras no se termina
-	while( 1 ) {
-		
-		// Mostrar operación
-		printf("1.- Suma\n");
-		printf("2.- Resta\n");
-		printf("3.- Multiplicacion\n");
-		printf("4.- Division\n");
-		printf("5.- Raiz Cuadrada\n");
-		printf("6.- Potencia\n");
-		printf("7.- Salir\n");
-		
-		// Leer operación
-		scanf("%d", &option);
-		
-		// Salir
-		if( option == 7 ) break;
-		
-		// Leer operandos
-		scanf("%d", &a);
-		scanf("%d", &b);
-		
-		// Operar
-		switch( option ) {
-			case 1:
-				result = 0;
-				break;
-			case 2:
-				result = 0;
-				break;
-			case 3:
-				result = 0;
-				break;
-			case 4:
-				result = 0;
-				break;
-			case 5:
-				result = 0;
-				break;
-			case 6:
-				result = 0;
-				break;
-			default:
-				result = 0;
-				printf("Opción incorrecta\n");
-				break;
-		}
-		
-		// Mostrar resultado
-		if( option > 0 && option < 7 ) {
-			printf("El resultado es %d", result);
-		}
-		
-	}
-	
-	// Salir sin error
-	return 0;
-}
 
+	int resultado; /* Variable reservada para cada resultado de las operaciones */
+
+	int a;         /* Primer numero que introducimos */
+
+	int b;         /* Segundo numero que introducimos */
+
+	int opcion;    /* Opcion que elegimos */
+
+	do
+
+	{
+
+		clrscr();
+
+		printf("\n         ÉÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍ»");
+
+		printf("\n         º               :: C A L C U L A D O R A ::                 º");
+
+		printf("\n         ÌÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍ¹");
+
+		printf("\n         º                                                           º");
+
+		printf("\n         º       1.- Suma                                            º");
+
+		printf("\n         º       2.- Resta                                           º");
+
+		printf("\n         º       3.- Multiplicacion                                  º");
+
+		printf("\n         º       4.- Division                                        º");
+
+		printf("\n         º       5.- Raiz Cuadrada                                   º");
+
+		printf("\n         º       6.- Potencia                                        º");
+
+		printf("\n         º       7.- Salir                                           º");
+
+		printf("\n         ÈÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍ¼");
+
+		printf("\n                     Nota: Solo funciona con numeros enteros");
+
+		printf("\n");
+
+		printf("\n                              Elija una opcion: ");
+
+		scanf("%d",&opcion);
+
+		switch(opcion)  /* Segun la opcion elegida tendremos los siguientes casos...*/
+
+		{
+
+			case 1:     /* Suma */
+
+			printf("\n         -> Introduzca el primer sumando: ");
+
+			scanf("%i",&a);
+
+			printf("\n         -> Introduzca el segundo sumando: ");
+
+			scanf("%i",&b);
+
+			resultado=a+b;
+
+			printf("\n         La SUMA de %i + %i es igual a %i",a,b,resultado);
+
+			break;
+
+ 
+
+			case 2:     /* Resta */
+
+			printf("\n         -> Introduzca el primer numero: ");
+
+			scanf("%i",&a);
+
+			printf("\n         -> Introduzca el segundo numero: ");
+
+			scanf("%i",&b);
+
+			resultado = a - b; 
+
+			printf("\n         La RESTA de %i - %i es igual a %i",a,b,resultado);
+
+			break;
+
+ 
+
+			case 3:     /* Raiz Cuadrada */
+
+			printf("\n         -> Introduzca el numero al que efectuar la raiz: ");
+
+			scanf("%i",&a);
+
+			
+
+				
+
+				printf("\n         La RAIZ CUADRADA de %i es igual a %i",a,resultado);
+
+			
+
+			break;
+
+ 
+
+			case 4:     /* Multiplicacion */
+
+			printf("\n         -> Introduzca el primer multiplicando: ");
+
+			scanf("%i",&a);
+
+			printf("\n         -> Introduzca el segundo multiplicando: ");
+
+			scanf("%i",&b);
+
+			
+
+			printf("\n         La MULTIPLICACION de %i * %i es igual a %i",a,b,resultado);
+
+			break;
+
+ 
+
+			case 5:     /* Division */
+
+			printf("\n         -> Introduzca el dividendo: ");
+
+			scanf("%i",&a);
+
+			printf("\n         -> Introduzca el divisor: ");
+
+			scanf("%i",&b);
+
+			
+
+				printf("\n         El cociente de la DIVISION de %i entre %i es igual a %i",a,b,resultado);
+
+			
+			
+			break;
+
+ 
+
+			case 6:
+
+			printf("\n         -> Introduzca el numero que hara de base: ");  /* Introducimos los datos */
+
+			scanf("%i",&a);
+
+			printf("\n         -> Introduzca el exponente: ");
+
+			scanf("%i",&b);
+
+			
+			
+			
+			
+			printf("\n         El resultado de %i^%i es %i",a,b,resultado);
+
+			}
+
+			break;
+
+ 
+
+			case 7:   /* Salir */
+
+		
+			cprintf("\n                            Pulse ENTER para salir...");
+
+			break;
+
+		} /* fin switch */
+
+	getch();
